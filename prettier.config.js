c37a7eea2b9211c31bee224e6d95545e7e15ea56
Ezequiel Saidman <ezemati1904@gmail.com>
2026-05-12 17:29:17 -0300

    Initial commit (TanStack Router)

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 120,
+};
+
+export default config;

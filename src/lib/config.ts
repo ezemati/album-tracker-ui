16cfd7fb018de12df166f18e0cf08538d1a8f825
Ezequiel Saidman <ezemati1904@gmail.com>
2026-05-14 14:44:38 -0300

    Don't ignore .env files

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-    VITE_API_BASE_URL: z.url().default('http://localhost:8000'),
+    VITE_API_BASE_URL: z.url().nonempty(),
 });
 
 export const config = envSchema.parse(import.meta.env);

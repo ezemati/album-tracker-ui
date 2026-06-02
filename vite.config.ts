c1f3b9148bc789eb41695e68cf133734e29ddc60
Ezequiel Saidman <ezequiel.saidman@innovid.com>
2026-06-01 23:31:39 -0300

    Install React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import viteReact from '@vitejs/plugin-react';
+import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 const config = defineConfig({
     resolve: { tsconfigPaths: true },
-    plugins: [devtools(), tailwindcss(), tanstackRouter({ target: 'react', autoCodeSplitting: true }), viteReact()],
+    plugins: [
+        devtools(),
+        tailwindcss(),
+        tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+        viteReact(),
+        babel({ presets: [reactCompilerPreset()] }),
+    ],
 });
 
 export default config;
